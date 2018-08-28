798b6578d085c501c235a7d09b5e53e5d4b1b8d8
Christian Bremer <christian.bremer@cstx.de>
2018-08-28 18:16:04 +0200

    Version 1.1.1-SNAPSHOT with cleaned code

--- src/main/java/org/bremersee/eureka/WebSecurityConfiguration.java
+++ src/main/java/org/bremersee/eureka/WebSecurityConfiguration.java
@@ -17,18 +17,14 @@
 package org.bremersee.eureka;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
-import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -37,7 +33,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
 /**
  * @author Christian Bremer
@@ -47,12 +42,7 @@ import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 @Slf4j
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  private static final Logger LOG = LoggerFactory.getLogger(WebSecurityConfiguration.class);
-
-  private static final String DEFAULT_ACCESS = "hasIpAddress('127.0.0.1') "
-      + "or hasIpAddress('::1') or isAuthenticated()";
-
-  private Environment env;
+  private final Environment env;
 
   private final WebSecurityProperties properties;
 
@@ -65,72 +55,28 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     final String appName = env.getProperty("spring.application.name", "eureka");
-    /*
     http
+        .authorizeRequests()
+        .requestMatchers(EndpointRequest.to(InfoEndpoint.class))
+        .permitAll()
+        .requestMatchers(EndpointRequest.to(HealthEndpoint.class))
+        .permitAll()
+        .requestMatchers(EndpointRequest.toAnyEndpoint())
+        .access(properties.getActuator().buildAccess())
+        .antMatchers("/**")
+        .access(properties.getApplication().buildAccess())
+        .and()
         .csrf().disable()
-        .httpBasic().realmName(env.getProperty("spring.application.name", "eureka"))
-        .and()
-        .authorizeRequests()
-        .anyRequest()
-        .access(env.getProperty("bremersee.access.default-access", DEFAULT_ACCESS));
-    */
-    /*
-    http
-        .requestMatcher(EndpointRequest.toAnyEndpoint())
-          .authorizeRequests()
-            .antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
-            .antMatchers(HttpMethod.GET, "/actuator/info").permitAll()
-            .anyRequest()
-            .access(properties.getActuator().buildAccess())
-        .and()
-          .requestMatcher(new NegatedRequestMatcher(EndpointRequest.toAnyEndpoint()))
-          .authorizeRequests()
-          .anyRequest()
-          .access(properties.getApplication().buildAccess())
-        .and()
-          .csrf().disable()
-          .userDetailsService(userDetailsService())
-          .httpBasic().realmName(appName);
-    */
-
-    http
-        .authorizeRequests()
-          .requestMatchers(EndpointRequest.to(InfoEndpoint.class))
-            .permitAll()
-          .requestMatchers(EndpointRequest.to(HealthEndpoint.class))
-            .permitAll()
-          .requestMatchers(EndpointRequest.toAnyEndpoint())
-            .access(properties.getActuator().buildAccess())
-          .antMatchers("/**")
-            .access(properties.getApplication().buildAccess())
-        .and()
-          .csrf().disable()
-          .userDetailsService(userDetailsService())
-          .httpBasic().realmName(appName);
+        .userDetailsService(userDetailsService())
+        .httpBasic().realmName(appName);
   }
 
-  /*
-  @Bean
-  @Override
-  public UserDetailsService userDetailsService() {
-
-    final String username = env.getProperty("spring.security.user.name", "user");
-    final String password = env.getProperty("spring.security.user.password", "changeit");
-    final String role = env.getProperty("spring.security.user.roles", "EUREKA");
-    LOG.debug("Login user: username = {}, password = {}, roles = {}",
-        username, password, role);
-
-    final PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    final UserDetails user = User.builder()
-        .username(username)
-        .password(password)
-        .roles(role)
-        .passwordEncoder(encoder::encode)
-        .build();
-    return new InMemoryUserDetailsManager(user);
-  }
-  */
-
+  /**
+   * Creates an in-memory user details service based on the users configured in {@link
+   * WebSecurityConfiguration}
+   *
+   * @return the user details service for the basic authentication
+   */
   @Bean
   @Override
   public UserDetailsService userDetailsService() {
